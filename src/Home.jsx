be0f24903868579c2ebf8ce7a2498085d3e0d92a
isa <166569877+Bel425366@users.noreply.github.com>
2025-05-19 12:08:34 +0000

    flex

---
+++ src/Home.jsx
@@ -0,0 +1,33 @@
+import poke from "./assets/pokeapi_256.png"
+import eevee from "./assets/eevee.jpg"
+import './Home.css'
+
+const Home = () => {
+    return(
+    <div className="pokeflex">
+        <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={eevee} />
+            <img src={poke} />
+            
+            
+        </div>
+    )
+}
+ 
+export default Home
+
